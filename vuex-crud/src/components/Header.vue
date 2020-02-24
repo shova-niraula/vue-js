fd306b5adf1218892ae1e02b4836d19316d202a1
Shova Niraula <shova.niraula@gmail.com>
2020-02-23 19:26:22 -0500

    Revert "deleted"
    This reverts commit 4dee03e43f300295805997d9791a2402ba838f5d.

---
+++ vuex-crud/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark" style="margin: 10px 0px 20px 0px">
+            <b-navbar-nav >
+                <b-nav-item to="/">Home</b-nav-item>
+                <b-nav-item to="/about">About</b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
